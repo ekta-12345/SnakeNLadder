8995d710d7dd6c7a9046b02ff61a3b52f02f6683
ekta12345 <ektakumbhare3@gmail.com>
2021-04-16 15:43:08 +0530

    Added code for single player Start position

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SnakeAndLadderAssignment
+{
+    class SnakeAndLadder
+    {
+        int START_POSITION = 0;
+        public void PlayGame()
+        {
+            Console.WriteLine(" Single Player Start_Position:- " + START_POSITION);
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            SnakeAndLadder StartPlay = new SnakeAndLadder();
+            StartPlay.PlayGame();
+
+        }
+    }
+}
