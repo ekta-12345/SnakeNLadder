f98dee16f014ecfe4a3d566d4c6bf49cfa740c7d
ekta12345 <ektakumbhare3@gmail.com>
2021-04-16 15:55:48 +0530

    Added code for player throwing the dice

--- Program.cs
+++ Program.cs
@@ -4,16 +4,19 @@ namespace SnakeAndLadderAssignment
 {
     class SnakeAndLadder
     {
-        int START_POSITION = 0;
+        int START_POSITION = 0; // Declare integer start position in 0
         public void PlayGame()
         {
-            Console.WriteLine(" Single Player Start_Position:- " + START_POSITION);
+            Random random = new Random();
+            int Die = random.Next(1, 6);    //Random Value Generate 1 to 6
+            Console.WriteLine(" Player Die Roll Position :-" + Die);        //Print Random value
+            Console.WriteLine(" Single Player Start_Position:- " + START_POSITION); //Print Start Position
             Console.ReadLine();
         }
-        static void Main(string[] args)
+        static void Main(string[] args)//Main method
         {
-            SnakeAndLadder StartPlay = new SnakeAndLadder();
-            StartPlay.PlayGame();
+            SnakeAndLadder StartPlay = new SnakeAndLadder(); // Calling Method
+            StartPlay.PlayGame();//method PlaGame
 
         }
     }
