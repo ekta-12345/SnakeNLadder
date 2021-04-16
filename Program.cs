65295e7ac005bcc4e39965cd7ccaab944cbbbe50
ekta12345 <ektakumbhare3@gmail.com>
2021-04-16 16:12:48 +0530

    Added code to ensure if the Player reaches the winning position

--- Program.cs
+++ Program.cs
@@ -8,33 +8,39 @@ namespace SnakeAndLadderAssignment
         const int NoPlay = 0;
         const int Ladder = 1;
         const int Snakle = 2;
+        const int winning_position = 100; // Declare Winning Position 100
         public void PlayGame()
         {
-            Random random = new Random(); //Generate random integers in range 0 to 6
-            int Die = random.Next(1, 6);    // Random Value Generate 1 to 6
-            int Option = random.Next(0, 3); // Random generate 0 to 3 value
             int Player = 0;
-            switch (Option) // use switch casw for option Random value
+            while (Player < winning_position)
             {
-                case NoPlay:
-                    Console.WriteLine("No Play, Pass the chance"); // Print  
-                    break;
-                case Ladder:
-                    Player = Die; // 
-                    Console.WriteLine("You got Ladder \n Player position = " + Player); // Print player value random genrate
-                    break;
-                case Snakle:
-                    Player -= Die;
-                    if (Player < START_POSITION) //Check Lessthan Start Position
-                    {
-                        Player = START_POSITION; // Player value = Start Value
-                    }
-                    Console.WriteLine("You gotSnake \n player position = " + Player);
+                Random random = new Random(); //Generate random integers in range 0 to 6
+                int Die = random.Next(1, 6);    // Random Value Generate 1 to 6
+                int Option = random.Next(0, 3); // Random generate 0 to 3 value
 
-                    break;
+
+                switch (Option) // use switch casw for option Random value
+                {
+                    case NoPlay:
+                        Console.WriteLine("No Play, Pass the chance"); // Print  
+                        break;
+                    case Ladder:
+                        Player += Die; // 
+                        Console.WriteLine("You got Ladder \n Player position = " + Player); // Print player value random genrate
+                        break;
+                    case Snakle:
+                        Player -= Die;
+                        if (Player < START_POSITION) //Check Lessthan Start Position
+                        {
+                            Player = START_POSITION; // Player value = Start Value
+                        }
+                        Console.WriteLine("You gotSnake \n player position = " + Player);
+
+                        break;
+                }
 
             }
-            Console.WriteLine(" Player Die Roll Position :- " + Die);                   //Print Random value
+            // Console.WriteLine(" Player Die Roll Position :- " + Die);                   //Print Random value
             Console.WriteLine(" Single Player Start_Position:- " + START_POSITION);     //Print Start Position
             Console.ReadLine();
         }
